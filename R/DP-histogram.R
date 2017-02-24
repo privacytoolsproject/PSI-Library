aca4538da64720b63977aa61ee71b429094a4d4d
Thomas Brawner <thomasbrawner@gmail.com>
2017-02-24 09:42:02 -0600

    Copy R files to package

---
+++ R/DP-histogram.R
@@ -0,0 +1,209 @@
+#source('utilities.R')
+#source('mechanisms.R')
+
+
+#' Release differentially private histogram 
+#' 
+#' @param x Vector, numeric or categorical
+#' @param var_type String, specifies the type of x
+#' @param range Tuple, range of x
+#' @param n Integer, number of observations in x
+#' @param epsilon Float, Epsilon value for differential privacy
+#' @param delta Float, Delta value for differential privacy
+#' @param beta Float, level of significance
+#' @param bins Vector of bins for which values are counted
+#' @param n_bins Integer, Number of cells in which to tabulate values in x, ignored if `var_type` \in {`factor`, `categorical`}
+#' @param mechanism String, Differentially private mechanism
+#'
+#' If the variable is categorical, bins are assumed to be provided by the depositor, and these bin values
+#' used to construct the table. The vector is pre-processed so that observed levels not specified in these
+#' bins are recoded to `NA`. Thus, any observed levels not specified in the `bins` argument show up as `NA` 
+#' in the output table.
+#'
+#' If the variable is numeric, the number of bins `n_bins` is set by the user optionally, else the Sturges
+#' method is used to select the number of bins given the number of observations `n`. The bins are then 
+#' constructed to be equal intervals between the provided range. 
+#'
+#' If the mechanism is not explicitly provided, use the mechanism with highest accuracy given the epsilon.
+#' 
+#' @examples
+#' # numeric types
+#' x_num <- rnorm(100)
+#' x_num_na <- x_num
+#' x_num_na[sample(1:length(x_num_na), size=10, replace=FALSE)] <- NA
+#' x_int <- as.integer(round(x_num * 20))
+#' r_num <- histogram.release(x_num, var_type='numeric', range=c(-2, 2), n=100, epsilon=0.1)
+#' r_num_na <- histogram.release(x_num_na, var_type='numeric', range=c(-2, 2), n=100, epsilon=0.1)
+#' r_int <- histogram.release(x_int, var_type='integer', range=c(-40, 40), n=100, epsilon=0.1)
+#' r_num_random <- histogram.release(x_num, var_type='numeric', range=c(-2, 2), n=100, epsilon=0.1, mechanism='random')
+#' # accuracy is returning inf, which filters the entire release for stability histogram 
+#' r_num_stability <- histogram.release(x_num, var_type='numeric', range=c(-2, 2), n=100, epsilon=0.1, mechanism='stability')
+#' r_num_noisy <- histogram.release(x_num, var_type='numeric', range=c(-2, 2), n=100, epsilon=0.1, mechanism='noisy')
+#'
+#' # categorical types
+#' x_char <- c(rep('a', 40), rep('b', 25), rep('c', 15), rep('d', 12), rep('e', 5), rep('f', 2), rep('g', 1))
+#' x_fac <- factor(x_char)
+#' bins <- c('a', 'b', 'c', 'd', 'e')
+#' r_char <- histogram.release(x_char, var_type='character', n=100, epsilon=0.1, bins=bins)
+#' r_fac <- histogram.release(x_fac, var_type='factor', n=100, epsilon=0.1, bins=bins)
+
+histogram.release <- function(x, var_type, n, epsilon, delta=2^-30, beta=0.05, range=NULL, bins=NULL, n_bins=NULL, mechanism=NULL) {
+
+    var_type <- check_variable_type(var_type, in_types=c('numeric', 'integer', 'factor', 'character'))
+
+    if (is.null(mechanism)) {
+        mechs <- c('noisy', 'stability', 'random')
+        accuracies <- c(
+            histogram.getAccuracy('noisy', n_bins, n, epsilon),
+            histogram.getAccuracy('stability', n_bins, n, epsilon),
+            histogram.getAccuracy('random', n_bins, n, epsilon)
+        )
+        mechanism <- mechs[which.min(accuracies)]
+    }
+    mechanism <- check_histogram_mechanism(mechanism)
+
+    if (mechanism == 'random') {
+
+        release <- mechanism.histogram.random(x, var_type, epsilon, levels, n_bins) 
+
+    } else {
+
+        if (var_type %in% c('factor', 'character')) {
+            
+            my_categorical_hist <- function(x, var_levels) {
+                n <- length(var_levels)  # avoid unused argument error in mechanism
+                histogram <- table(x, useNA='ifany')
+                return(histogram)
+            }
+            release <- mechanism.laplace(fun=my_categorical_hist, x=x, var_type=var_type, range=range, sensitivity=2, epsilon=epsilon, var_levels=bins)
+        
+        } else { 
+
+            my_numeric_hist <- function(x, var_levels) {
+                histogram <- table(cut(x, breaks=var_levels, include.lowest=TRUE, right=TRUE))
+                return(histogram)
+            }
+            n_bins <- check_histogram_bins(n_bins=n_bins, n=n)
+            var_levels <- seq(range[1], range[2], length.out=(n_bins + 1))
+            release <- mechanism.laplace(fun=my_numeric_hist, x=x, var_type=var_type, range=range, sensitivity=2, epsilon=epsilon, var_levels=var_levels)
+        } 
+
+        if (mechanism == 'noisy') {
+
+            release <- ifelse(release < 0, 0, round(release))
+            
+        } else {
+            
+            accuracy <- histogram.getAccuracy(mechanism, n_bins, n, epsilon)
+            if (check_histogram_n(accuracy, n, n_bins, epsilon, delta, beta)) {
+                a <- accuracy * n / 2
+                release <- release[release >= a]
+            }
+        }
+    }
+
+    return(release)
+}
+
+
+#' Accuracy of release 
+#' 
+#' @param mechanism Specify the histogram mechanism 
+#' @param n_bins Number of cells in which to tabulate values 
+#' @param epsilon Epsilon value for differential privacy
+#' @param delta Delta value for differential privacy
+#' @param beta
+
+histogram.getAccuracy <- function(mechanism, n_bins, n, epsilon, delta=2^-30, beta=0.05, error=1e-9) { 
+    mechanism <- check_histogram_mechanism(mechanism)
+
+    if (mechanism == 'noisy') { 
+        acc <- -2 * log(1 - (1 - beta)^(1 / n_bins)) / (n * epsilon)
+    } else if (mechanism == 'stability') { 
+        lo <- 0
+        hi <- 1
+        eval <- beta + error
+        while ((eval <= beta - error) || (eval > beta)) { 
+            acc <- (hi + lo) / 2
+            eval <- min((4 / acc), n_bins) * exp(-acc * n * epsilon / 4)
+            if (eval < beta) { 
+                hi <- acc
+            } else { 
+                lo <- acc
+            } 
+            if (hi - lo <= 0) { 
+                return(Inf)
+            } 
+        } 
+        acc <- max(acc, (8 / n) * (0.5 - log(delta) / epsilon))
+    } else { 
+        acc <- Inf
+    } 
+    return(acc) 
+} 
+
+
+#' Privacy parameters
+#'
+#' @param mechanism Specify the histogram mechanism
+#' @param n_bins Number of cells in which to tabulate values
+#' @param accuracy
+#' @param delta Delta value for differential privacy
+#' @param beta
+#' @return epsilon Differential privacy parameter
+
+histogram.getParameters <- function(mechanism, n_bins, n, accuracy, delta, beta=0.05, error=1e-9) {
+    mechanism <- check_histogram_mechanism(mechanism)
+
+    if (mechanism == 'noisy') {
+        eps <- -2 * log(1 - (1 - beta)^(1 / n_bins)) / (n * accuracy)
+    } else if (mechanism == 'stability') {
+        lo <- 0
+        hi <- 1
+        eval <- n + 1
+        while ((eval <= n * (1 - error)) || (eval > n)) {
+            eps <- (hi + lo) / 2
+            eval <- 8 / accuracy * (0.5 - log(delta) / eps)
+            if (eval < n) {
+                hi <- eps
+            } else {
+                lo <- eps
+            }
+            if (hi - lo <= 0) {
+                return(Inf)
+            }
+        }
+        eps <- max(eps, 4 * log(min(n_bins, 4 / accuracy) / beta) / (accuracy * n))
+    } else {
+        eps <- log((n_bins + 1) / (n_bins - 1))
+    }
+    return(eps)
+}
+
+
+#' JSON doc for histogram
+#'
+#' @return JSON for histogram function
+
+histogram.getJSON <- function(output.json=TRUE) {
+    out <- list()
+    out$statistic <- 'Histogram'
+    out$description <- 'Differentially Private Histogram'
+    out$mechanisms <- c('noisy', 'random', 'stability')
+    out$variableTypes <- list('numeric' = list(), 'categorical' = list())
+    out$variableTypes$numeric$rTypes <- c('numeric', 'integer')
+    out$variableTypes$numeric$fields <- list(
+            'n' = 'Number of observations',
+            'range' = 'Ordered pair indicating effective lower and upper bounds',
+            'n_bins' = 'Number of cells in output (optional, default Sturges method)'
+        )
+    out$variableTypes$categorical$rTypes <- c('character', 'factor')
+    out$variableTypes$categorical$fields <- list(
+            'n' = 'Number of observations',
+            'bins' = 'Vector indicating levels for which to produce frequencies'
+        )
+    if (output.json) {
+        out <- jsonlite::toJSON(out, pretty=TRUE)
+    }
+    return(out)
+}
---
+++ R/utilities.R
@@ -0,0 +1,237 @@
+#' Random draw from Laplace distribution
+#'
+#' @param sensitivity numeric 
+#' @param epsilon numeric
+#' @param n integer, number of draws
+#' @return Random draws from Laplace distribution
+#' @examples
+#' rlaplace(sensitivity=1, epsilon=0.1)
+
+rlaplace = function(n=1, sensitivity, epsilon) { 
+    flip <- sample(c(-1, 1), size=n, replace=TRUE)
+    expon <- rexp(n=n, rate=(epsilon / sensitivity))
+    return(flip * expon)
+}
+
+
+#' Random draw from Laplace distribution
+#'
+#' @param loc numeric, center of the distribution
+#' @param scale numeric, spread
+#' @param n integer, number of draws
+#' @return Random draws from Laplace distribution
+#' @examples
+#' rlap(size=1000)
+
+rlap = function(loc=0, scale=1, size=1) { 
+    p <- runif(size)
+    sgn <- sample(c(-1, 1), size=size, replace=TRUE)
+    draws <- loc - scale * sgn * (p - 0.5) * log(1 - 2 * abs(p - 0.5))
+    return(draws)
+}
+
+
+#' Probability density for Laplace distribution 
+#'
+#' @param x numeric, value 
+#' @param loc numeric, center of the distribution
+#' @param scale numeric, spread
+#' @return Density for elements of x
+#' @examples
+#' x <- seq(-3, 3, length.out=61)
+#' dlap(x)
+
+dlap <- function(x, loc=0, scale=1) { 
+    dens <- 0.5 * scale * exp(-1 * abs(x - loc) / scale)    
+    return(dens)
+} 
+
+
+#' Utility function for checking that range is ordered pair
+#'
+#' @param range A vector, that ought to be an ordered pair
+#' @return An ordered pair 
+#'
+#' Checks if a supplied range is an ordered pair.  Coerces any vector of length two
+#'   or greater into an ordered pair, and issues an error for shorter vectors.
+#'
+#' @examples
+#' checkrange(1:3)
+#' \dontrun{checkrange(1)}
+
+checkrange = function(range){
+	if (length(range)<2){
+		stop("range argument in error: requires upper and lower values as vector of length 2.")
+	} else if (length(range)>2){
+		warning("range argument supplied has more than two values.  Will proceed using min and max values as range.")
+		range <- c(min(range),max(range))
+	}else{
+		range <- sort(range)
+	}
+	return(range)
+}
+
+
+#' Utility function for checking that epsilon is acceptably defined
+#'
+#' @param epsilon A vector, that ought to be positive and length of 1
+#' @return The supplied epsilon if acceptable, otherwise an error message interupts
+#'
+#' @examples
+#' checkepsilon(0.1)
+#' \dontrun{checkepsilon(-2)}
+#' \dontrun{checkepsilon(c(0.1,0.5))}
+
+checkepsilon = function(epsilon){
+	if (epsilon<=0){
+		stop("Privacy parameter epsilon must be a value greater than zero.")
+	}
+	if (length(epsilon)>1){
+		stop(paste("Privacy parameter epsilon must be a single value, but is currently a vector of length ",length(epsilon)) )
+	}
+	return(epsilon)
+}
+
+
+#' Utility function for censoring data
+#'
+#' @param x A vector of values to censor within a range
+#' @param range A vector (min, max) of the bounds of the range
+#' @return Original vector with values outide the bounds of the range censored to the bounds
+#'
+#' Checks if val is in range = (min, max)
+#'   and censors values to either min or max if it is 
+#'   out of the range
+#'
+#' @examples
+#' censordata(x=1:10, var_type='integer', range=c(2.5,7)) 
+
+censordata = function(x, var_type, range=NULL, levels=NULL) {
+
+    if (var_type %in% c('character', 'factor')) {
+        if (is.null(levels)) {
+            stop('`levels` are required for categorical types')
+        }
+        x <- factor(x, levels=levels, exclude=NULL)
+    } else {
+        if (is.null(range)) {
+            stop('`range` is required for numeric types')
+        }
+        range <- checkrange(range)
+        x[x < range[1]] <- range[1]
+        x[x > range[2]] <- range[2]
+    }
+    return(x)
+}
+
+
+#' Utility function to check type of variable is within set of acceptable types 
+#'
+#' @param type String specifying the type of the variable
+#' @param in_types Vector of acceptable types 
+#' 
+#' Verifies that the variable is an element in the set of acceptable types
+#' 
+#' @examples 
+#' check_variable_type(type='Numeric', in_types=c('Numeric', 'Factor'))
+
+check_variable_type = function(type, in_types) { 
+    if (!(type %in% in_types)) {
+        stop(paste('Variable type', type, 'should be one of', paste(in_types, collapse = ', ')))
+    } 
+    return(type)
+} 
+
+
+#' Utility function to verify that a variable is dichotomous
+#'
+#' @param x Vector of values
+#' @return Logical vector coded 0-1
+#'
+#' This function effectively allows the user to ask for any variable containing
+#' at most two unique values to treat the variable as logical. If the variable
+#' contains numeric values, the highest value is recoded 1 and the the lower
+#' value is recoded 0. If the variable is categorical and contains only two unique
+#' values, the least frequently observed is recoded 1.
+#'
+#' @examples
+#' make_logical(sample(c('cat', 'dog'), size=8, replace=TRUE))
+#' make_logical(sample(c(0, 1), size=8, replace=TRUE))
+#' make_logical(sample(c(-6.87, 3.23), size=8, replace=TRUE)
+
+make_logical <- function(x) {
+    if (!length(unique(x)) <= 2) { # how to handle if contains 1 value only?
+        stop('Variable has more than two values')
+    }
+    if (class(x) %in% c('character', 'factor')) {
+        tab <- data.frame(table(x))
+        min_level <- tab[which.min(tab[, 2]), 1]
+        x <- ifelse(x == min_level, 1, 0)
+    } else {
+        x <- ifelse(x == max(x), 1, 0)
+    }
+    return(x)
+}
+
+
+#' Utility function to verify the type of histogram mechanism
+#' 
+#' @param mechanism Character string specifying the mechanism
+#' 
+#' Verifies that the mechanism is one of `noisy`, `stability`, or `random` and returns 
+#' the mechanism if so, else throws an error 
+#' 
+#' @examples 
+#' check_histogram_mechanism('stability')
+
+check_histogram_mechanism <- function(mechanism) { 
+    if (!(is.null(mechanism)) && !(mechanism %in% c('noisy', 'stability', 'random'))) { 
+        stop('`mechanism` must be one of `noisy`, `stability`, `random`')
+    } 
+    return(mechanism)
+} 
+
+
+#' Utility function to include NA level for categorical types when vector of bins
+#' does not include all observed levels in the data vector.
+#'
+#' @param x Vector, categorical type
+#' @param bins Vector, depositor-provided list of levels for which to count values
+
+check_histogram_categorical <- function(x, bins) {
+    x <- factor(x, levels=bins, exclude=NULL)
+    return(x)
+}
+
+
+#' Utility function to check bins argument to histogram
+#' 
+#' @param n_bins Number of cells in which to tabulate values
+#' @param n Number of observations
+#' 
+#' If number of bins is not provided, use the Sturges method
+
+check_histogram_bins <- function(n_bins, n) {
+    if (is.null(n_bins)) {
+        n_bins <- ceiling(log2(n)) + 1
+    } else if (n_bins < 2) {
+        stop('`n_bins` must be at least 2')
+    } else if (as.logical(n_bins %% 1)) {
+        warning('non-integer value `n_bins` converted to next highest integer value')
+        n_bins <- ceiling(n_bins)
+    }
+    return(n_bins)
+}
+
+
+#' Utility function to check sufficient n 
+#' 
+
+check_histogram_n <- function(accuracy, n, n_bins, epsilon, delta, beta) { 
+    cond1 <- (8 / accuracy) * (0.5 - log(delta) / epsilon)
+    cond2 <- 4 * log(min(n_bins, (4 / accuracy)) / beta) / (accuracy * epsilon)
+    if (n < max(cond1, cond2, na.rm=TRUE)) { 
+        stop('number of rows insufficient to provide privacy or accuracy with given parameters')
+    } 
+    return(TRUE)
+} 
