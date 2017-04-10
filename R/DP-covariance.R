6ba84b85a848b517410e60ba69482abf21acf27b
Thomas Brawner <thomasbrawner@gmail.com>
2017-04-10 10:08:27 -0500

    Add algorithm for DP covariance

--- R/DP-covariance.R
+++ R/DP-covariance.R
@@ -6,36 +6,84 @@
 #' @param trim.thresh Numeric, default 0.95
 
 dp.covariance <- function(z, n, epsilon, trim.thresh=0.95) {
+    return(list('z' = z,
+                'n' = n,
+                'epsilon' = epsilon,
+                'trim.thresh' = trim.thresh))
+}
 
-    # features
-    features <- names(z)
 
-    # euclidean norm
-    z$dist <- apply(z, 1, function(x) { sqrt(sum(x^2)) } )
+if (interactive()) {
 
-    # sort increasing by distance
-    z <- z[order(z$dist), ]
+    library(mvtnorm)
 
-    # evaluate sensitivity
-    threshold <- n * trim.thresh
-    sensitivity <- z$dist[threshold + 1] - z$dist[threshold - 1]
+    # fake data
+    N <- 5000
+    d <- 10
+    vcov <- matrix(0, nrow=d, ncol=d)
+    diag(vcov) <- 1
+    vcov[1:3, 1:3] <- c(1.0, 0.3, 0.15, 0.3, 1.0, 0.1, 0.15, 0.1, 1.0)
 
-    # add Laplace noise to radius
-    threshold <- threshold + rlap(b=(sensitivity / epsilon))
+    # draw some data & bound
+    df <- as.data.frame(rmvnorm(n=N, mean=rep(0, d), sigma=vcov))
+    intercept <- rep(1, N)
+    lower.bound <- -4.0
+    upper.bound <- 4.0
+    df[df < lower.bound] <- lower.bound
+    df[df > upper.bound] <- upper.bound
+    df <- cbind(1, df)
+    names(df) <- c('intercept', 'y', paste0('x', 1:(d-1)))
+    features <- names(df)
 
-    # trim z
-    z <- z[1:threshold, features]
+    # trim details
+    trim.thresh <- 0.95
+    n.trim <- N * trim.thresh
 
-    # noisy z'z
-    cov.mat <- cov(z)
-    noise <- rlap(b=(sensitivity / epsilon), size=(nrow(cov.mat) * ncol(cov.mat)))
-    dim(noise) <- dim(cov.mat)
-    release <- cov.mat + noise
+    # covariance routine
+    df$dist <- apply(df, 1, function(x) { max(abs(x)) } )
+    df <- df[order(df$dist, decreasing=TRUE), features]
+    df.trim <- as.matrix(df[1:n.trim, features])
 
-    out <- list(
-        'threshold' = threshold,
-        'sensitivity' = sensitivity,
-        'noise' = noise,
-        'release' = release)
-    return(out)
+    # inner product
+    inner <- function(x) {
+        return(t(x) %*% x)
+    }
+
+    # covariance
+    vc <- inner(as.matrix(df))
+    vc.trim <- inner(df.trim)
+
+    # largest possible sensitivity
+    l2.bound <- sqrt((upper.bound - lower.bound)^2)
+    delta <- 2e-16
+    epsilon <- 0.1
+
+    the.matrix <- matrix(NA, nrow=nrow(vc), ncol=ncol(vc))
+    for (row in 1:nrow(vc)) {
+        n.draws <- nrow(vc) - row + 1
+        draws <- rnorm(n.draws, mean=0, sd=(l2.bound^2 * sqrt(2 * log(1.25 / delta) / epsilon)))
+        the.matrix[row, row:nrow(vc)] <- the.matrix[row:nrow(vc), row] <- draws
+    }
+
+    # jackknife the vc matrix for local sensitivity
+    vc.lower <- vc.upper <- inner(df.trim[1, , drop=FALSE])
+    for (row in 2:nrow(df.trim)) {
+        row.inner <- inner(df.trim[row, , drop=FALSE])
+        below.flag <- row.inner < vc.lower
+        above.flag <- row.inner > vc.upper
+        vc.lower[below.flag] <- row.inner[below.flag]
+        vc.upper[above.flag] <- row.inner[above.flag]
+    }
+    jack.vc.diff <- vc.upper - vc.lower
+
+    trim.matrix <- matrix(NA, nrow=nrow(vc.trim), ncol=ncol(vc.trim))
+    for(row in 1:nrow(vc.trim)) {
+        for (col in row:ncol(vc.trim)) {
+            noise <- rnorm(1, mean=0, sd=jack.vc.diff[row, col] * sqrt(2 * log(1.25 / delta) / epsilon))
+            trim.matrix[row, col] <- trim.matrix[col, row] <- noise
+        }
+    }
+
+    priv <- vc + the.matrix
+    priv.trim <- vc.trim + trim.matrix
 }
