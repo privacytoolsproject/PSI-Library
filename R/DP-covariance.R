a495e2984a45f7edcbcd1b14864618cadefe18a3
Thomas Brawner <thomasbrawner@gmail.com>
2017-05-04 11:24:46 -0500

    Add revised covariance function using Laplace mechanism

--- R/DP-covariance.R
+++ R/DP-covariance.R
@@ -2,28 +2,22 @@
 #'
 #' @param x Numeric data frame
 #' @param n Integer indicating the number of observations
-#' @param rng Numeric 2-tuple, lower and upper bounds for standardized data
+#' @param rng Numeric matrix of 2-tuples with the lower and upper bounds for each of P variables, dimensions Px2
 #' @param epsilon Numeric differential privacy parameter epsilon
-#' @param columns Character vector indicating columns in \code{x}, if NULL then use all columns
-#' @param trim.thresh Numeric, default 0.95
+#' @param columns Character vector indicating columns in \code{x}
+#' @param intercept Logical indicating whether an intercept should be added prior to evaluating the inner product x'x
 
-dp.covariance <- function(x, n, rng, epsilon, columns, intercept, trim, trim.thresh) {
+dp.covariance <- function(x, n, rng, epsilon, columns, intercept) {
 
     # subset and optionally append an intercept
     data <- x[, columns]
     if (intercept) {
         data <- cbind(1, data)
-    }
-
-    # trim the extreme values
-    if (trim) {
-        dist <- order(apply(data, 1, function(x) { max(abs(x)) } ))
-        n.trim <- as.integer(trim.thresh * n)
-        data <- data[1:n.trim, ]
+        columns <- c('intercept', columns)
     }
 
     # the true statistic, return only the lower triangle
-    covariance <- cov(data)
+    covariance <- t(as.matrix(data)) %*% as.matrix(data)
     lower <- lower.tri(covariance, diag=TRUE)
     covariance <- covariance[lower]
 
@@ -32,25 +26,39 @@ dp.covariance <- function(x, n, rng, epsilon, columns, intercept, trim, trim.thr
                 'n' = n,
                 'rng' = rng,
                 'epsilon' = epsilon,
-                'trim.thresh' = trim.thresh))
+                'columns' = columns))
 }
 
 
+#' Function to release a differentially private inner product of input matrix
+#'
+#' @param x Numeric data frame, dimensions NxP
+#' @param var.type Numeric variable types
+#' @param n Integer indicating the number of observations
+#' @param epsilon Numeric differential privacy parameter
+#' @param rng Numeric matrix of 2-tuples with the lower and upper bounds for each of P variables, dimensions Px2
+#' @param columns Character vector indicating columns in \code{x}
+#' @param intercept Logical indicating whether an intercept should be added prior to evaluating the inner product x'x, default to FALSE
 
-covariance.release <- function(x, var.type, n, epsilon, rng, columns, delta=2e-16, intercept=FALSE, trim=TRUE, trim.thresh=0.95) {
+covariance.release <- function(x, var.type, n, epsilon, rng, columns, intercept=FALSE) {
 
-    if (intercept) {
-        columns <- c('intercept', columns)
-        rng <- rbind(c(1, 1), rng)
+    # get the vector of sensitivities from the ranges
+    diffs = apply(rng, 1, diff)
+    if (intercept) { diffs <- c(1, diffs) }
+    diffs.combns <- c()
+    for (i in 1:length(diffs)) {
+        for (j in i:length(diffs)) {
+            diffs.combns <- c(diffs.combns, c(diffs[i], diffs[j]))
+        }
     }
-    sensitivity <- 2 * length(columns)
-    postlist <- NULL
+    diffs = matrix(diffs.combns, ncol=2, byrow=TRUE)
+    sensitivity <- ((n - 1) / n) * diffs[, 1] * diffs[, 2]
 
     # pass to mechanism
+    postlist <- NULL
     release <- mechanism.laplace(fun=dp.covariance, x=x, var.type='numeric', n=n,
-                                 rng=rng, epsilon=(n * epsilon), columns=columns,
-                                 sensitivity=sensitivity, intercept=intercept, trim=trim,
-                                 trim.thresh=trim.thresh, postlist=postlist)
+                                 rng=rng, epsilon=(epsilon / length(sensitivity)), columns=columns,
+                                 sensitivity=sensitivity, intercept=intercept, postlist=postlist)
 
     # convert to symmetric matrix
     out.dim <- ifelse(intercept, length(columns) + 1, length(columns))
@@ -58,14 +66,14 @@ covariance.release <- function(x, var.type, n, epsilon, rng, columns, delta=2e-1
     out.matrix[lower.tri(out.matrix, diag=TRUE)] <- release$release
     out.matrix[upper.tri(out.matrix, diag=FALSE)] <- t(out.matrix)[upper.tri(out.matrix, diag=FALSE)]
     release$release <- data.frame(out.matrix)
-    rownames(release$release) <- names(release$release) <- columns
+    rownames(release$release) <- names(release$release) <- release$columns
     return(release)
 }
 
 
 if (interactive()) {
 
-    set.seed(2)
+    set.seed(681521)
     n <- 10000
     x1 <- rnorm(n, mean=73, sd=17)
     x2 <- rpois(n, lambda=4)
@@ -77,5 +85,12 @@ if (interactive()) {
     rng <- rbind(range(y), range(x1), range(x2), range(x3))
     dtypes <- 'numeric'
     eps <- 0.5
+
     release <- covariance.release(df, dtypes, n, eps, rng, cols)
+    observe <- t(as.matrix(df)) %*% as.matrix(df)
+
+    release2 <- covariance.release(df, dtypes, n, eps, rng, cols, intercept=TRUE)
+    df2 <- cbind(1, df)
+    names(df2) <- c('intercept', names(df))
+    observe2 <- t(as.matrix(df2)) %*% as.matrix(df2)
 }
