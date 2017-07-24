8f2c3ebba8dcc47176ac15196ea79da974bb288b
CHRISTIAN BAEHR <CBAEHR@WUSTL.EDU>
2017-07-24 10:06:58 -0400

    Some added documentation to utilities

--- R/utilities.R
+++ R/utilities.R
@@ -1,16 +1,21 @@
-#' Draw cryptographically secure random variates from uniform distribution
+#' Differentially Private Uniform Draw 
+#' 
+#' Draw cryptographically secure random variates from a uniform distribution.
 #'
-#' @param n Integer giving number of variates needed
-#' @param seed Integer indicating a seed for R's PNRG, defaults to \code{NULL}
+#' @param n An integer giving number of variates needed.
+#' @param seed An integer indicating a seed for R's PNRG, defaults to \code{NULL}.
+#' 
 #' @return Random numeric vector of length \code{n} containing values between
 #'    zero and one.
 #'
 #' Draws secure random variates from the uniform distribution through \code{openssl}.
 #' If a seed is provided, the \code{runif} function is used to draw the random variates.
-#'
 #' @examples
+#' 
 #' uniform_secure <- dpUnif(n=1000)
 #' uniform_repeatable <- dpUnif(n=1, seed=75436)
+#' @seealso \code{\link{dpNoise}}
+#' @rdname dpUnif
 #' @export
 dpUnif <- function(n, seed=NULL) {
     if (!is.null(seed)) {
@@ -21,17 +26,26 @@ dpUnif <- function(n, seed=NULL) {
 }
 
 
-#' Draw cryptographically secure random variates
+#' Differentially Private Noise Generator
+#' 
+#' Compile noise from a cryptographically secure random variates to achieve
+#'    differentially private statistics.
 #'
-#' @param n Integer giving number of variates needed
-#' @param scale Numeric scale for the distribution
-#' @param dist Character specifying the distribution from which to draw the noise
-#' @param seed Integer indicating a seed for R's PNRG, defaults to \code{NULL}
-#'
+#' @param n An integer giving number of variates needed.
+#' @param scale Numeric, the scale for the distribution.
+#' @param dist A character specifying the distribution from which to draw the 
+#'    noise.
+#' @param seed An integer indicating a seed for R's PNRG, defaults 
+#'    to \code{NULL}.
+#'    
+#' @return Cryptographically secure noise vector or matrix.
 #' @examples
+#' 
 #' laplace_noise <- dpNoise(n=1000, scale=1, dist='laplace')
 #' gaussian_noise <- dpNoise(n=1000, scale=1, dist='gaussian')
 #' laplace_noise_repeatable <- dpNoise(n=1, scale=1, dist='laplace', seed=96845)
+#' @seealso \code{\link{dpUnif}}
+#' @rdname dpNoise
 #' @export
 dpNoise <- function(n, scale, dist, seed=NULL) {
     u <- dpUnif(n, seed)
@@ -43,7 +57,6 @@ dpNoise <- function(n, scale, dist, seed=NULL) {
         stop(sprintf('Distribution "%s" not understood', dist))
     }
 }
-
 
 
 #' Random draw from Laplace distribution
@@ -94,15 +107,21 @@ dlap <- function(x, mu=0, b=1) {
 }
 
 
-#' Cumulative distribution function for Laplace distribution
+#' LaPlace Cumulative Distribution Function
+#' 
+#' Determines the probability a draw from a LaPlace distribution is less than 
+#'    or equal to the specified value.
 #'
-#' @param x numeric, value
-#' @param mu numeric, center of the distribution
-#' @param b numeric, spread
-#' @return Probability less than or equal to x
+#' @param x Numeric, the value(s) at which the user wants to know the CDF height.
+#' @param mu Numeric, the center of the LaPlace distribution, defaults to 0.
+#' @param b Numeric, the spread of the LaPlace distribution, defaults to 1.
+#' 
+#' @return Probability the LaPlace draw is less than or equal to \code{x}.
 #' @examples
+#' 
 #' x <- 0
 #' plap(x)
+#' @rdname plap
 #' @export
 plap <- function(x, mu=0, b=1) {
     cdf <- 0.5 + 0.5 * sgn(x - mu) * (1 - exp(-1 * (abs(x - mu) / b)))
@@ -138,17 +157,22 @@ sgn <- function(x) {
 }
 
 
-#' Utility function for checking that range is ordered pair
-#'
-#' @param range A vector, that ought to be an ordered pair
-#' @return An ordered pair
-#'
-#' Checks if a supplied range is an ordered pair.  Coerces any vector of length two
-#'   or greater into an ordered pair, and issues an error for shorter vectors.
-#'
+#' Range Parameter Check
+#' 
+#' Checks if a supplied range is an ordered pair. Coerces any vector of length 
+#'    two or greater into an ordered pair, and issues an error for
+#'    shorter vectors.
+#'    
+#' @param range A numeric vector of length two, that ought to be an 
+#'    ordered pair.
+#' 
+#' @return An ordered pair.
 #' @examples
+#'
+#' checkrange(c(1,3))
 #' checkrange(1:3)
 #' \dontrun{checkrange(1)}
+#' @rdname checkrange
 #' @export
 checkrange <- function(rng) {
     if (NCOL(rng) > 1) {
@@ -169,17 +193,23 @@ checkrange <- function(rng) {
 }
 
 
-#' Utility function for checking that epsilon is acceptably defined
+#' Epsilon Parameter Check
+#' 
+#' Utility function for checking that epsilon is acceptably defined.
 #'
-#' @param epsilon A vector, that ought to be positive and length of 1
-#' @return The supplied epsilon if acceptable, otherwise an error message interupts
+#' @param epsilon A vector, that ought to be positive and of length one.
+#' 
+#' @return The supplied epsilon if acceptable, otherwise an error 
+#'    message interupts.
 #'
 #' @examples
+#' 
 #' checkepsilon(0.1)
 #' \dontrun{checkepsilon(-2)}
 #' \dontrun{checkepsilon(c(0.1,0.5))}
+#' @rdname checkepsilon
 #' @export
-checkepsilon = function(epsilon) {
+checkepsilon <- function(epsilon) {
 	if (epsilon <= 0) {
 		stop("Privacy parameter epsilon must be a value greater than zero.")
 	}
@@ -190,22 +220,28 @@ checkepsilon = function(epsilon) {
 }
 
 
-#' Utility function for censoring data
+#' Censoring data
+#' 
+#' For numeric types, checks if x is in rng = (min, max) and censors values to 
+#'    either min or max if it is out of the range. For categorical types, 
+#'    values not in `levels` are coded NA.
 #'
-#' @param x A vector of numeric or categorial values to censor
-#' @param var_type Character indicating the variable type
-#' @param rng For numeric vectors, a vector (min, max) of the bounds of the range. For numeric matrices with nrow N and ncol P, a Px2 matrix of (min, max) bounds.
-#' @param levels For categorical types, a vector containing the levels to be returned
-#' @return Original vector with values outside the bounds censored to the bounds
-#'
-#' For numeric types, checks if x is in rng = (min, max) and censors values to either min
-#' or max if it is out of the range. For categorical types, values not in `levels` are coded NA.
-#'
+#' @param x A vector of numeric or categorial values to censor.
+#' @param var_type Character indicating the variable type of \code{x}.
+#' @param rng For numeric vectors, a vector (min, max) of the bounds of the 
+#'    range. For numeric matrices with nrow N and ncol P, a Px2 matrix of 
+#'    (min, max) bounds.
+#' @param levels For categorical types, a vector containing the levels to 
+#'    be returned.
+#' 
+#' @return Original vector with values outside the bounds censored to the bounds.
 #' @examples
+#' 
 #' censordata(x=1:10, var_type='integer', rng=c(2.5, 7))
 #' censordata(x=c('a', 'b', 'c', 'd'), var_type='character', levels=c('a', 'b', 'c'))
+#' @rdname censordata
 #' @export
-censordata = function(x, var_type, rng=NULL, levels=NULL) {
+censordata <- function(x, var_type, rng=NULL, levels=NULL) {
     if (var_type %in% c('character', 'factor')) {
         if (is.null(levels)) {
             stop('`levels` are required for categorical types')
@@ -231,18 +267,20 @@ censordata = function(x, var_type, rng=NULL, levels=NULL) {
 }
 
 
-#' Utility function to check type of variable is within set of acceptable types 
-#'
-#' @param type Character specifying the type of the variable
-#' @param in_types Vector of acceptable types 
-#' @return The original character string indicating the variable type
+#' Checking variable types
 #' 
-#' Verifies that the variable is an element in the set of acceptable types
+#' Verifies that the variable is an element in the set of acceptable types.
 #' 
+#' @param type A character specifying the type of the variable.
+#' @param in_types A vector of acceptable types of variables.
+#' 
+#' @return The original character string indicating the variable type.
 #' @examples 
+#' 
 #' check_variable_type(type='Numeric', in_types=c('Numeric', 'Factor'))
+#' @rdname check_variable_type
 #' @export
-check_variable_type = function(type, in_types) { 
+check_variable_type <- function(type, in_types) { 
     if (!(type %in% in_types)) {
         stop(paste('Variable type', type, 'should be one of', paste(in_types, collapse = ', ')))
     } 
@@ -250,10 +288,13 @@ check_variable_type = function(type, in_types) {
 } 
 
 
-#' Utility function to verify that a variable is dichotomous
+#' Logical variable check
+#' 
+#' Utility function to verify that a variable is dichotomous.
 #'
-#' @param x Vector of values
-#' @return Logical vector coded 0-1
+#' @param x Vector containing two unique types of values.
+#' 
+#' @return Logical form of \code{x} coded 0-1.
 #'
 #' This function effectively allows the user to ask for any variable containing
 #' at most two unique values to treat the variable as logical. If the variable
@@ -262,9 +303,11 @@ check_variable_type = function(type, in_types) {
 #' values, the least frequently observed is recoded 1.
 #'
 #' @examples
+#' 
 #' make_logical(sample(c('cat', 'dog'), size=8, replace=TRUE))
 #' make_logical(sample(c(0, 1), size=8, replace=TRUE))
 #' make_logical(sample(c(-6.87, 3.23), size=8, replace=TRUE))
+#' @rdname make_logical
 #' @export
 make_logical <- function(x) {
     if (!length(unique(x)) <= 2) { # how to handle if contains 1 value only?
