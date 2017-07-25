6ccdbd17215fe94a5ec3a33351caf0d559b487dc
Thomas Brawner <thomasbrawner@gmail.com>
2017-07-25 10:21:53 -0500

    Apply noise to objectives separately with bootstrap

--- R/mechanisms.R
+++ R/mechanisms.R
@@ -119,23 +119,23 @@ mechanism.objective <- function(fun, x, n, epsilon, n.boot, ...) {
     scaler <- mapMatrixUnit(X, p=2)
     X <- scaler$matrix
 
-    # noise
-    if (!is.null(n.boot)) { epsilon <- epsilon / n.boot }
-    b.norm <- dpNoise(n=1, scale=(2 / epsilon), dist='gamma', shape=ncol(X))
-    b <- dpNoise(n=ncol(X), scale=(-epsilon * b.norm), dist='laplace')
-
     # fit and adjust back to original scale
     start.params <- rep(0, ncol(X))
     if (is.null(n.boot)) {
+        b.norm <- dpNoise(n=1, scale=(2 / epsilon), dist='gamma', shape=ncol(X))
+        b <- dpNoise(n=ncol(X), scale=(-epsilon * b.norm), dist='laplace')
         release <- data.frame(optim(par=start.params, fn=out$objective, X=X, y=y, b=b, n=n)$par / scaler$max.norm)
         names(release) <- 'estimate'
         rownames(release) <- X.names
     } else {
+        epsilon <- epsilon / n.boot
         boot.ests <- vector('list', n.boot)
         for (i in 1:n.boot) {
             index <- sample(1:n, n, replace=TRUE)
             X.star <- X[index, ]
             y.star <- y[index]
+            b.norm <- dpNoise(n=1, scale=(2 / epsilon), dist='gamma', shape=ncol(X))
+            b <- dpNoise(n=ncol(X), scale=(-epsilon * b.norm), dist='laplace')
             boot.ests[[i]] <- optim(par=start.params, fn=out$objective, X=X.star, y=y.star, b=b, n=n)$par / scaler$max.norm
         }
         release <- data.frame(do.call(rbind, boot.ests))
