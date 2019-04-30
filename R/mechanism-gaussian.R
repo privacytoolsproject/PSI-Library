28a8fb3f55c4d95875a6960115b5a4f97b2d4d8b
Ira Globus-Harris <igh@bu.edu>
2019-04-30 17:21:50 -0400

    fixing gaussian mech

--- R/mechanism-gaussian.R
+++ R/mechanism-gaussian.R
@@ -17,7 +17,7 @@ mechanismGaussian$methods(
         x <- fillMissing(x, .self$var.type, impute.rng=.self$rng, categories=.self$bins)
         fun.args <- getFuncArgs(fun, inputList=list(...), inputObject=.self)
         input.vals = c(list(x=x), fun.args)
-        true.val <- do.call(fun, c(list(x=x), field.vals))
+        true.val <- do.call(fun, input.vals)
         scale <- sens * sqrt(2 * log(1.25 / .self$delta)) / .self$epsilon
         release <- true.val + dpNoise(n=length(true.val), scale=scale, dist='gaussian')
         out <- list('release' = release)
