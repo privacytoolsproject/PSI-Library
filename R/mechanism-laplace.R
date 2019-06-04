9b8a1ef43626de458090dd4ad11abc2cb6334e7a
Megan Fantes <meganfantes@gmail.com>
2019-06-04 17:31:25 -0400

    setting up PSIlence on my computer

--- R/mechanism-laplace.R
+++ R/mechanism-laplace.R
@@ -17,7 +17,6 @@ mechanismLaplace$methods(
   #' Differentially private evaluation of input function "fun" with sensitivity "sens" on input data 
   #' "x" using the Laplace mechanism.
   #' 
-  #' @name Laplace Mechanism
   #' @references C. Dwork, A. Roth The Algorithmic Foundations of Differential Privacy, Chapter 3.3 The Laplace Mechanism p.30-37. August 2014.
   #'
   #' @param fun function of input x to add Laplace noise to.
