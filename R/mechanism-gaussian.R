14f7175ad6aef3d7390310f16c25007f918d3112
Megan Fantes <meganfantes@gmail.com>
2019-06-10 16:43:36 -0400

    In both the Laplace and Gaussian mechanisms, in the evaluate method, add an if statement to ensure that noise is only being added to bins that are not empty in a stability mechanism (in a non-stability mechanism, noise is still added to all bins)

--- R/mechanism-gaussian.R
+++ R/mechanism-gaussian.R
@@ -12,14 +12,23 @@ mechanismGaussian <- setRefClass(
 )
 
 mechanismGaussian$methods(
-    evaluate = function(fun, x, sens, postFun, ...) {
+    evaluate = function(fun, x, sens, postFun, stability, ...) {
         x <- censordata(x, .self$var.type, .self$rng)
         x <- fillMissing(x, .self$var.type, impute.rng=.self$rng, categories=.self$bins)
         fun.args <- getFuncArgs(fun, inputList=list(...), inputObject=.self)
         input.vals = c(list(x=x), fun.args)
         true.val <- do.call(fun, input.vals)
         scale <- sens * sqrt(2 * log(1.25 / .self$delta)) / .self$epsilon
-        release <- true.val + dpNoise(n=length(true.val), scale=scale, dist='gaussian')
+        # release <- true.val + dpNoise(n=length(true.val), scale=scale, dist='gaussian')
+        noiseVector <- dpNoise(n=length(true.val), scale=scale, dist='gaussian')
+        if (stability) {
+            # if this is for a stability mechanism, only add noise to bins
+            # that are NOT equal to zero
+            release <- ifelse(true.val == 0, true.val, true.val + noiseVector)
+        } else {
+            # if this is NOT stability mechanism, add noise to all bins
+            release <- true.val + noiseVector
+        }
         out <- list('release' = release)
         out <- postFun(out, ...)
         return(out)
