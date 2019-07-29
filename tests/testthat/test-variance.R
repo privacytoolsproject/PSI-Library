9208e062ddd7fba81dac11600da78f4963da75cf
Megan Fantes <meganfantes@gmail.com>
2019-07-29 12:31:36 -0400

    Make sure checkrange() returns correct errors. Issue #72.

--- tests/testthat/test-variance.R
+++ tests/testthat/test-variance.R
@@ -7,9 +7,9 @@ data(PUMS5extract10000)
 test_that('error thrown when n not positive or whole number', {
     my_epsilon <- 0.1
     my_delta <- 10^-6
-    expect_error(dpVariance$new(mechanism='mechanismLaplace', variable='age', var.type='numeric', n=-1, epsilon=my_epsilon, rng=c(100)),
+    expect_error(dpVariance$new(mechanism='mechanismLaplace', variable='age', var.type='numeric', n=-1, epsilon=my_epsilon, rng=c(18,93)),
                  "n must be a positive whole number")
-    expect_error(dpMean$new(mechanism='mechanismLaplace', variable='age', var.type='numeric', n=0.5, epsilon=my_epsilon, rng=c(-10,0,100)),
+    expect_error(dpMean$new(mechanism='mechanismLaplace', variable='age', var.type='numeric', n=0.5, epsilon=my_epsilon, rng=c(18,93)),
                  "n must be a positive whole number")
 })
 
@@ -23,6 +23,25 @@ test_that('range checks throw correct warning', {
     dp.variance <- dpVariance$new(mechanism='mechanismLaplace', variable='sex', var.type='logical', n=my_n, epsilon=my_epsilon)
     dp.variance$release(PUMS5extract10000)
     
+    expect_equal(length(dp.variance$result$release), 1)
+    expect_equal(dp.variance$epsilon, my_epsilon)
+})
+
+# make sure error is thrown when dimension of range entered is incorrect
+test_that('range checks throw correct warning', {
+    data(PUMS5extract10000, package = "PSIlence")
+    
+    my_n <- 10000
+    my_epsilon <- 0.1
+    my_delta <- 10^-6
+    
+    expect_error(dpVariance$new(mechanism='mechanismLaplace', variable='age', var.type='numeric', n=my_n, epsilon=my_epsilon, rng=c(100)), 
+                 "range argument in error: requires upper and lower values as vector of length 2.")
+    expect_warning(dpMean$new(mechanism='mechanismLaplace', variable='age', var.type='numeric', n=my_n, epsilon=my_epsilon, rng=c(-10,0,100)), 
+                   "range argument supplied has more than two values.  Will proceed using min and max values as range.")
+    
+    dp.variance <- dpVariance$new(mechanism='mechanismLaplace', variable='age', var.type='numeric', n=my_n, epsilon=my_epsilon, rng=c(0,100))
+    dp.variance$release(PUMS5extract10000)
     expect_equal(length(dp.variance$result$release), 1)
     expect_equal(dp.variance$epsilon, my_epsilon)
 })
