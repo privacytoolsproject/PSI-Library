daaa729cfdf5850f2d71114116e0ef5d04a332ac
Ira Globus-Harris <igh@bu.edu>
2019-08-12 16:32:50 -0400

    changed variance sensitivity. Do not merge until Megan's refactoring is done

--- R/statistic-variance.R
+++ R/statistic-variance.R
@@ -1,3 +1,16 @@
+#' Sensitivity of population variance
+#' 
+#' For a detailed derivation of the sensitivity, see /extra_docs/variance_sensitivity.pdf.
+#' @param n Numeric vector of length one; the number of datapoints in the database.
+#' @param rng Numeric vector of length two; first entry is minimal bound on the database entries, second is maximal bound on the database entries.
+#' @return Numeric vector of length one; a maximal bound on the sensitivity of the population variance.
+#'
+#' @examples
+#' varianceSensitivity(2,c(0,10)) #should return 50
+varianceSensitivity <- function(n, rng){
+  return(diff(rng)^2/n)
+}
+
 #' Postprocessed variance standard deviation
 #' 
 #' Function to extract standard deviation from noisy estimate of variance.
@@ -5,9 +18,9 @@
 #' @param release Differentially private release of variance.
 #' 
 #' @return Noisy estimate of the standard deviation of \code{release}.
-#' @rdname variance.postStandardDeviation
+#' @rdname postStandDev
 
-variance.postStandardDeviation <- function(release) {
+postStandDev <- function(release) {
     std <- sqrt(release)
     return(std)
 }
@@ -16,14 +29,14 @@ variance.postStandardDeviation <- function(release) {
 #' Differentially private variance
 #'
 #' @param mechanism Character, the privacy mechanism.
-#' @param var.type Character, the R variable type. One of \code{c('numeric',
+#' @param varType Character, the R variable type. One of \code{c('numeric',
 #'   'integer', 'logical')}.
 #' @param Variable Character, variable name.
 #' @param n Integer, number of observations
 #' @param rng Numeric, a priori estimate of the range
 #' @param epsilon Numeric, privacy cost parameter
 #' @param accuracy Numeric, accuracy guarantee given \code{epsilon}
-#' @param impute.rng Numeric, range within which missing values are imputed. If \code{NULL},
+#' @param imputeRng Numeric, range within which missing values are imputed. If \code{NULL},
 #'   the range provided in \code{rng} is used.
 #' @param alpha Numeric, the level of statistical significance. Default 0.05.
 #'
@@ -40,29 +53,29 @@ dpVariance <- setRefClass(
 )
 
 dpVariance$methods(
-    initialize = function(mechanism, var.type, variable, n, rng=NULL, epsilon,
-                          impute.rng=NULL, alpha=0.05) {
+    initialize = function(mechanism, varType, variable, n, rng=NULL, epsilon,
+                          imputeRng=NULL, alpha=0.05) {
         .self$name <- 'Differentially private variance'
         .self$mechanism <- mechanism
-        .self$var.type <- var.type
+        .self$varType <- varType
         .self$variable <- variable
         .self$n <- checkNValidity(n)
-        .self$rng <- checkRange(rng, var.type)
-        .self$sens <- (n - 1) / n^2 * diff(rng)^2
+        .self$rng <- checkRange(rng, .self$varType)
+        .self$sens <- varianceSensitivity(.self$n, .self$rng)
         
         if (is.null(epsilon)) {
             .self$accuracy <- accuracy
-            .self$epsilon <- laplace.getEpsilon(.self$sens, .self$accuracy, alpha)
+            .self$epsilon <- laplaceGetEpsilon(.self$sens, .self$accuracy, alpha)
         } else {
             checkEpsilon(epsilon)
             .self$epsilon <- epsilon
             .self$accuracy <- laplaceGetAccuracy(.self$sens, .self$epsilon, alpha)
         }
         
-        if (is.null(impute.rng)) {
-            .self$impute.rng <- rng
+        if (is.null(imputeRng)) {
+            .self$imputeRng <- rng
         } else {
-            .self$impute.rng <- checkImputationRange(impute.rng, .self$rng, .self$var.type)
+            .self$imputeRng <- checkImputationRange(imputeRng, .self$rng, .self$varType)
         }
         
         .self$alpha <- alpha
@@ -77,6 +90,6 @@ dpVariance$methods(
 dpVariance$methods(
     postProcess = function(out) {
         out$variable <- variable
-        out$std <- sqrt(out$release)
+        out$std <- postStandDev(out$release)
         return(out)
 })
