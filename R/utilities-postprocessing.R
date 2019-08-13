2a981c648f96160561e2153d9debfa72cc0fa539
Megan Fantes <meganfantes@gmail.com>
2019-08-13 17:24:07 -0400

    move createJSON function to utilities-postprocessing.R

--- R/utilities-postprocessing.R
+++ R/utilities-postprocessing.R
@@ -41,6 +41,27 @@ release2json <- function(release, namesList){
     
 }
 
+#' Function to create JSON file defining differentially private statistics
+
+createJSON <- function() { 
+    
+    statistics <- list(
+        'histogram', 
+        'mean'
+    )
+    
+    statJSON <- function(stat) {
+        func <- match.fun(paste0(stat, '.getJSON'))
+        out <- list() 
+        out[[stat]] <- func(output.json=FALSE) 
+        return(out)
+    } 
+    
+    json.list <- list('DP Statistics' = lapply(statistics, statJSON))
+    cat(jsonlite::toJSON(json.list, pretty=TRUE), '\n', file=file.path('DP-statistics.json'))
+    return(TRUE)
+}
+
 
 #' Function to trim lower and upper regions of a vector of values
 #'
