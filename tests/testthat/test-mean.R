2ab7f05f0d166fcb29843909b98b149def142af0
Megan Fantes <meganfantes@gmail.com>
2019-08-14 11:10:40 -0400

    add tests for accuracy and epsilon to mean test file

--- tests/testthat/test-mean.R
+++ tests/testthat/test-mean.R
@@ -1,6 +1,31 @@
 library(PSIlence)
 context("mean")
 
+# test accuracy, epsilon, and sensitivity calculations 
+test_that('variancee getAccuracy and getEpsilon return approximately correct values for laplace mechanism', {
+    # test sensitivity and accuracy
+    nTest <- 10000
+    epsilonTest <- 0.1
+    
+    dpMeanTest <- dpMean$new(mechanism='mechanismLaplace', variable='age', varType='numeric', n=nTest, epsilon=epsilonTest, rng=c(0,100))
+    dpMeanTest$release(PUMS5extract10000)
+    
+    sens <- 100 / nTest
+    acc <- round(dpMeanTest$result$accuracy, digits = 1)
+
+    expect_equal(sens, 0.01)
+    expect_equal(acc, 0.3)
+    
+    # test accuracy
+    accuracyTest <- 0.3
+
+    dpMeanTest2 <- dpMean$new(mechanism='mechanismLaplace', variable='age', varType='numeric', n=nTest, accuracy=accuracyTest, rng=c(0,100))
+    dpMeanTest2$release(PUMS5extract10000)
+
+    epsilon <- round(dpMeanTest2$result$epsilon, digits = 1)
+    expect_equal(epsilon, 0.1)
+})
+
 # make sure range checks throw correct warning
 test_that('range checks throw correct warning', {
   data(PUMS5extract10000, package = "PSIlence")
