18b1ef8ffddd2e13cb25c0229a31472cf1e25636
Ira Globus-Harris <igh@bu.edu>
2019-09-09 13:21:40 -0400

    Covariance fixes and testing

--- tests/testthat/test-covariance.R
+++ tests/testthat/test-covariance.R
@@ -3,57 +3,79 @@ context("covariance")
 
 data(PUMS5extract10000)
 
+######################### Input validation tests #######################################################
+
 test_that('epsilon checks throw correct warning', {
-  rangeIncome <- c(-10000, 713000)
-  rangeEducation <- c(1, 16)
-  range <- rbind(rangeIncome, rangeEducation)
-  
-  expect_error(dpCovariance$new(mechanism="mechanismLaplace",varType = 'numeric', n = 10000, 
-                                epsilon = -0.1, columns = c("income", "education"), rng = range),
-               "Privacy parameter epsilon must be a value greater than zero.")
-  
-  expect_error(dpCovariance$new(mechanism="mechanismLaplace",varType = 'numeric', n = 10000, 
-                                epsilon = c(0.1, 0.5), columns = c("income", "education"), rng = range),
-               "Privacy parameter epsilon must be a single value, but is currently a vector of length 2")
+    rangeIncome <- c(-10000, 713000)
+    rangeEducation <- c(1, 16)
+    range <- rbind(rangeIncome, rangeEducation)
+    
+    expect_error(dpCovariance$new(mechanism="mechanismLaplace",varType = 'numeric', n = 10000, 
+                                  epsilon = -0.1, columns = c("income", "education"), rng = range),
+                 "Privacy parameter epsilon must be a value greater than zero.")
 })
 
 test_that('range checks throw correct warning', {
-  expect_error(dpCovariance$new(mechanism='mechanismLaplace', varType='numeric', n= 10000,
-                                epsilon = 0.1, columns = c("income", "education"),
-                                rng=list(c(100))))
-  
-  expect_error(dpCovariance$new(mechanism='mechanismLaplace', varType='numeric', n=10000,
-                                 epsilon=0.1, columns = c("income", "education"),
-                                 rng=list(c(-10,0,100))),
-               "Input was expected to be of length  2  but is instead of length  1", fixed=TRUE)
-})
-
-test_that('covariance running as expected', {
-  skip("Skipping: covariance does not run as expected.")
-  rangeIncome <- c(-10000, 713000)
-  rangeEducation <- c(1, 16)
-  range <- rbind(rangeIncome, rangeEducation)
-  
-  dpCov <- dpCovariance$new(mechanism="mechanismLaplace",varType = 'numeric', n = 10000, 
-                            epsilon = 5000000000, columns = c("income", "educ"), rng = range, formula='x~y')
-  expect_silent(dpCov$release(PUMS5extract10000))
-  print(dpCov$release(PUMS5extract10000))
-  
-  print("expected values")
-  print(cov(x=data["income"], y=data["educ"]))
-  print(var(data["income"]))
-  print(var(data["educ"]))
-  
-  print(range(data["income"]))
-  print(range(data["educ"]))
+    rng <- c(100)
+    rngStr <- paste('c(',toString(rng),')')
+    errorStr <- paste('Error in range argument provided,', rngStr, ': requires upper and lower values as vector of length 2.')
+    
+    #fixed=TRUE forces error string to be interpreted as fixed string rather than regular expression, which is necessary due to parenthesis in error message.
+    expect_error(dpCovariance$new(mechanism='mechanismLaplace', varType='numeric', n= 10000,
+                                  epsilon = 0.1, columns = c("income", "education"),
+                                  rng=rng),
+                 errorStr, fixed=TRUE) 
+    
+    rng <- matrix(c(-10,0,100), nrow=1)
+    rngStr <- paste('c(', toString(rng), ')')
+    warningStr <- paste('Range argument of', rngStr, 'has more than two values.  Will proceed using min and max values as range.')
+    
+    #fixed=TRUE forces warning string to be interpreted as fixed string rather than regular expression, which is necessary due to parenthesis in error message.
+    expect_warning(dpCovariance$new(mechanism='mechanismLaplace', varType='numeric', n=10000,
+                                    epsilon=0.1, columns = c("income", "education"),
+                                    rng=rng),
+                   warningStr, fixed=TRUE)
 })
 
 # make sure error thrown when n not positive or a whole number
-test_that('error thrown when n not positive or whole number', {
+test_that('make sure error thrown when n not positive or a whole number',{
     expect_error(dpCovariance$new(mechanism="mechanismLaplace",varType = 'numeric', n = -1, 
                                   epsilon = 1, columns = c("income", "educ"), rng = range, formula='x~y'),
                  "n must be a positive whole number")
     expect_error(dpCovariance$new(mechanism="mechanismLaplace",varType = 'numeric', n = 0.5, 
                                   epsilon = 1, columns = c("income", "educ"), rng = range, formula='x~y'),
                  "n must be a positive whole number")
+})
+
+######################### Workflow tests #######################################################
+
+test_that('DP covariance workflow runs', {
+    range.income <- range(PUMS5extract10000['income'])
+    range.education <- range(PUMS5extract10000['educ'])
+    range.age <- range(PUMS5extract10000['age'])
+    range <- rbind(range.income, range.education, range.age)
+    
+    dpCov <- dpCovariance$new(mechanism="mechanismLaplace",varType = 'numeric', n = 10000,
+                              epsilon = 1, columns = c("income", "educ", 'age'), rng = range, formula='income~educ')
+    out <- dpCov$release(PUMS5extract10000)
+    expect_equal(length(out),3)
+})
+
+test_that('coefficient release function operational in workflow', {
+    range.income <- range(PUMS5extract10000['income'])
+    range.education <- range(PUMS5extract10000['educ'])
+    range.age <- range(PUMS5extract10000['age'])
+    range <- rbind(range.income, range.education, range.age)
+    
+    #Next line expected to throw warning due to high epsilon val.
+    dpCov <- dpCovariance$new(mechanism="mechanismLaplace",varType = 'numeric', n = 10000,
+                              epsilon = 10000000000, columns = c("income", "educ", "age"), rng = range, formula='income~educ')
+    out <- dpCov$release(PUMS5extract10000)
+    coeffs <- coefficientRelease('income~age', out$release, n=10000)
+    expect_equal(length(coeffs), 4)
+    linreg <- lm(income~age, data=PUMS5extract10000)
+    
+    output <- as.numeric(coeffs$coefficients[[1]][1]) #extracts coefficient from output
+    expectedOutput <- as.numeric(linreg[[1]][2])
+    expect_equal(floor(output), floor(expectedOutput)) #check floor of values due to fact that there is some noise added here
 })
