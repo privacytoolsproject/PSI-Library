3990d1a9c4617aad462aa7eda9004125c03b84a8
Ira Globus-Harris <igh@bu.edu>
2019-10-18 18:47:30 -0400

    tree postprocessing and some testing

--- tests/testthat/test-utilities-tree.R
+++ tests/testthat/test-utilities-tree.R
@@ -1,1 +1,11 @@
 context('tree utils')
+
+test_that('adjacent element flip', {
+  ls <- c(1,2,3,4)
+  expect_equal(adjacentElements(ls), c(2,1,4,3))
+})
+
+test_that('estimation from below runs',{
+  t <- list(c(10), c(6,4), c(3,3,1,3))
+  
+})
