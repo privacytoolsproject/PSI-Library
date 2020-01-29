e3f7d0b278cf3c8fd6e2a530e1285d4b2ceafe38
Ira Globus-Harris <igh@bu.edu>
2020-01-29 15:09:02 -0500

    branch to save heavyhitters statistic on. do not delete but also don't merge without changing to make DP

--- R/mechanism-exponential.R
+++ R/mechanism-exponential.R
@@ -17,11 +17,11 @@ mechanismExponential$methods(
         x <- fillMissing(x, .self$varType, rng=.self$rng, categories=.self$bins)
         fun.args <- getFuncArgs(fun, inputList=list(...), inputObject=.self)
         inputVals = c(list(x=x), fun.args)
-        trueVal <- do.call(fun, inputVals)  # Concern: are we confident that the environment this is happening in is getting erased.
+        trueVal <- do.call(fun, inputVals)
         quality <- trueVal - max(trueVal)
         probs <- ifelse(trueVal == 0, 0, exp((.self$epsilon * quality) / (2 * sens)))
         gap <- as.numeric(trueVal[.self$k] - trueVal[.self$k + 1])
-        if (gap < (-2 / .self$epsilon * log(.self$delta))) {
+        if (gap < (-2 / .self$epsilon * log(.self$delta))) { # THIS IS NOT DIFFERENTIALLY PRIVATE SINCE THE GAP IS NOT DP!
             out <- list('release' = NULL)
         } else {
             release <- sample(names(trueVal), size=.self$k, prob=probs)
