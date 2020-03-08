6a4cb6533e60486628abb7e00be2bc4cdcc45c6f
ellenk <ekraffmiller@hmdc.harvard.edu>
2020-03-08 09:25:15 -0400

    Add tolerance to expect_equal test

--- tests/testthat/test-unbiasedprivacy.R
+++ tests/testthat/test-unbiasedprivacy.R
@@ -8,7 +8,7 @@ test_that('test current sim', {
    # param_combos <- read.csv('ellen_param_combos.csv')
    # param_row <- param_combos[5,]
     
-#TODO - generate random seed?
+    #TODO - generate random seed?
  
     param_row <- list( N = 25000, 
                       sim=2,
@@ -27,21 +27,28 @@ test_that('test current sim', {
     # run UDP sim
     save_path = '.'
     returnValue = udpSim(param_row, save_path = save_path)
-    
-   # expect_equal(returnValue$theta_tilde, 3)
+    expect_equal(returnValue$theta_tilde, 2.998053, tolerance=1e-5)
     #adjusted solution is in theta_tilde
    
 })
 
 #test_that('unbiased privacy sanity check', {
     # instantiate sample and aggregate for Mean with one subset and normal dpMean calculation
-#    unbiased_privacy_test <- dpUnbiasedPrivacy(innerFun = 'mean', aggregationFun = 'Mean', numSubsets = 1,
-#                                                      mechanism = 'mechanismLaplace', varType = 'numeric',
-#                                                      variable = 'income', n = nrow(PUMS5extract10000), epsilon = 10^6, rng = c(0, 750000))
-#    mean_test <- dpMean(mechanism = 'mechanismLaplace', varType = 'numeric',
-#                                                      variable = 'income', n = nrow(PUMS5extract10000), epsilon = 10^6, rng = c(0, 750000))
-    # release results
-#    unbiased_privacy_test$release(data)
+#    pr <- param_row
+#    dat <- generateData(pr$N, pr$intercept, pr$beta, pr$y_var, seed = pr$seed) # generate new dataset
+
+    # Calculate lambda based on specified value of alpha (prop. censoring) and OLS SE
+#    l <- trueLambdaCalc(pr$beta, dat$X, pr$P, pr$y_var, pr$alpha)
+    #true_sigma <- se * sqrt(pr$P)
+
+    # QOI
+#    form <- as.formula(Y1~X)
+#    coef <- 'X'
+
+#    sim <- algorithmUDP(data = dat, statistic = coefFn, B = pr$R, n = pr$b, P = pr$P, lambda = l, lambda_var = 0.025, delta = 0.01,
+#                    epsilon = pr$e, epsilon_alpha = pr$e_alpha, parallelize = F, censoring_cutoff = 0.9,
+#                    bias_cutoff = 0.1, form = form, coef = coef)
+
    
 #})
 
